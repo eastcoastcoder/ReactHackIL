0ca53565f55a4032dfc825a421dcccd86844934b
Ethan Richardson <ethanx94@gmail.com>
2017-02-25 20:48:58 -0600

    added fetch

--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+var APIKey = 'cd1aa174e078484bbda23afd59b452e3';
+var APISecret = '19135914982590CE96592961FDA62C0FC209AC423BF3915B669A118AC26D992B';
+var buf = new Buffer(APIKey + ":" + APISecret).toString('base64');
+
 var people = [
 
 	{'fName':'Jen', 'lName':'Earl', 'problems':[]},
@@ -20,5 +24,18 @@ var people = [
 	{'fName':'Sam', 'lName':'Tenant', 'problems':[]},
 	{'fName':'Dad', 'lName':'Earl', 'problems':[]}
 ];//ajax in the future
+console.log(buf);
+fetch('https://ipl-nonproduction-customer_validation.e-imo.com/api/v3/actions/categorize', { 
+	method: 'POST',
+	headers: {
+		'Authorization': 'Basic ' + buf,
+		'Content-Type': 'application/json'    
+	}
+	})
+	.then(function(response) {
+		return response.json()
+	}).then(function(body) {
+		console.log(body);
+	});
 
 ReactDOM.render(<Patient person = {people[1]}/>, document.getElementById('buttons'));
